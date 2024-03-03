3fba2e934be467cdda4a5adb894b6180a9bc533b
Abbas Parseh <148212563+parseh3347@users.noreply.github.com>
2024-03-03 11:30:01 +0330

    create fiel greeting

---
+++ dataentry/management/commands/greeting.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'greetings'
+
+    def add_arguments(self, parser):
+        parser.add_argument('name' , type=str ,help='specifi name')
+
+    def handle(self, *args,  **kwargs):
+        name=kwargs['name']
+        greeting = f'hi {name} how are you'
+        self.stdout.write(greeting)

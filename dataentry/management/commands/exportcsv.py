fcb4d7412d2899476711d657a58aa66c44ceac48
Abbas Parseh <148212563+parseh3347@users.noreply.github.com>
2024-03-07 00:08:02 +0330

    create file exportcsv

--- dataentry/management/commands/exportcsv.py
+++ dataentry/management/commands/exportcsv.py
@@ -6,20 +6,21 @@ from datetime import datetime
 
 class Command(BaseCommand):   
     help= 'data export to file csv'
+
+    students= Student.objects.all()
+
     def handle(self,*args,**kwargs):
-        students = Student.objects.all()
+        timemap=datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        datefile=f'{timemap}'
 
-        timetamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-        filepath = f'export_data_to {timetamp}.csv'
+        typefile = [('csv','*.csv'),('txt','*.txt')]
+        f=filedialog.asksaveasfilename(filetypes=typefile,defaultextension='.csv')
 
-        filetype=[('csv','*.csv'),('txt','*.txt',)]
-        f=filedialog.asksaveasfilename(filetypes=filetype,defaultextension='.csv')
+        with open(f,'ta',newline='') as file:
+            writer=csv.writer(file)
+            writer.writerow(['serial_number','name','emil','date'])
 
-        with open(f,'w',newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['code','username','email'])
-
-            for student in students:
-                writer.writerow([student.code, student.username, student.email])
-
-        self.stdout.write(self.style.SUCCESS('data export successfully!'))
+            for student in self.students:
+                writer.writerow([student.code,student.username,student.email,datefile])
+        
+        self.stdout.write(self.style.SUCCESS('data export sucessfully'))
